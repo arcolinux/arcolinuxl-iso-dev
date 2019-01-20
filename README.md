a29eeb2ed447269e5e93c554aeb101c0ef92b088
Erik Dubois <erik.dubois@gmail.com>
2019-01-20 17:31:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# arcolinux-iso-developement
+
+With this github you can build your own ArcoLinux version.
+
+Check out this link for more info : 
+
+https://arcolinuxd.com/build-your-own-iso-on-arcolinux-vision/
---
+++ installation-scripts/30-build-the-iso-local.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo
+echo "################################################################## "
+echo "Phase 1 : get the latest bashrc from github"
+echo "################################################################## "
+echo
+echo "Removing old files/folders from folder"
+rm -rf ../archiso/airootfs/etc/skel/.* 2> /dev/null
+echo "getting .bashrc from arcolinux-root"
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/root/.bashrc-latest -O ../archiso/airootfs/etc/skel/.bashrc
+echo ".bashrc copied to /etc/skel"
+
+
+echo
+echo "################################################################## "
+echo "Phase 2 : Checking if archiso is installed"
+echo "################################################################## "
+echo
+echo "Checking if archiso is installed"
+
+package="archiso"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+		echo "################################################################"
+		echo "################## "$package" is already installed"
+		echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi yay &> /dev/null; then
+
+		echo "################################################################"
+		echo "######### Installing with yay"
+		echo "################################################################"
+		yay -S --noconfirm $package
+
+	elif pacman -Qi trizen &> /dev/null; then
+
+		echo "################################################################"
+		echo "######### Installing with trizen"
+		echo "################################################################"
+		trizen -S --noconfirm --needed --noedit $package
+
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "################################################################"
+		echo "######### Installing with yaourt"
+		echo "################################################################"
+		yaourt -S --noconfirm $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+
+		echo "################################################################"
+		echo "######### Installing with pacaur"
+		echo "################################################################"
+		pacaur -S --noconfirm --noedit  $package
+
+	elif pacman -Qi packer &> /dev/null; then
+
+		echo "################################################################"
+		echo "######### Installing with packer"
+		echo "################################################################"
+		packer -S --noconfirm --noedit  $package
+
+	fi
+
+	# Just checking if installation was successful
+	if pacman -Qi $package &> /dev/null; then
+
+		echo "################################################################"
+		echo "#########  "$package" has been installed"
+		echo "################################################################"
+
+	else
+
+		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		echo "!!!!!!!!!  "$package" has NOT been installed"
+		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+	fi
+
+fi
+
+
+echo
+echo "################################################################## "
+echo "Phase 3 : Making sure we start with a clean slate"
+echo "################################################################## "
+echo
+echo "Deleting the build folder if one exists - takes some time"
+[ -d ~/arcolinux-build ] && sudo rm -rf ~/arcolinux-build
+
+
+echo
+echo "################################################################## "
+echo "Phase 4 : Moving files to arcolinux-build folder"
+echo "################################################################## "
+echo
+echo "Copying files and folder to ~/arcolinux-build"
+sudo cp -r ../../arcolinux-iso ~/arcolinux-build
+
+sudo chmod 750 ~/arcolinux-build/archiso/airootfs/etc/sudoers.d
+sudo chmod 750 ~/arcolinux-build/archiso/airootfs/etc/polkit-1/rules.d
+sudo chgrp polkitd ~/arcolinux-build/archiso/airootfs/etc/polkit-1/rules.d
+
+echo
+echo "################################################################## "
+echo "Phase 5 : Cleaning the cache"
+echo "################################################################## "
+echo
+yes | sudo pacman -Scc
+
+
+echo
+echo "################################################################## "
+echo "Phase 6 : Building the iso"
+echo "################################################################## "
+echo
+
+cd ~/arcolinux-build/archiso/
+sudo ./build.sh -v
+
+echo
+echo "################################################################## "
+echo "Phase 7 : Copying the iso to ~/ArcoLinux-Out"
+echo "################################################################## "
+echo
+[ -d  ~/ArcoLinux-Out ] || mkdir ~/ArcoLinux-Out
+cp ~/arcolinux-build/archiso/out/arcolinux* ~/ArcoLinux-Out
