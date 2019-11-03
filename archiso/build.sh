f539143b985ab1a546c683f2c9d1e5aa2e2aa3cf
Erik Dubois <erik.dubois@gmail.com>
2019-11-03 08:27:02 +0100

    fix for lightdm

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-dev
-iso_label="arcolinux-dev-v19.12.1"
+iso_label="arcolinux-dev-v19.12.3"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v19.12.1"
+iso_version="v19.12.3"
 install_dir=arch
 work_dir=work
 out_dir=out
