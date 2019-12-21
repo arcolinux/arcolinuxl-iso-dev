4a9e8809f6806cf46afa109d6a10c73d204a5c24
Erik Dubois <erik.dubois@gmail.com>
2019-12-21 15:01:49 +0100

    moving to /usr/local/bin

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -8,5 +8,6 @@ rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rm /etc/mkinitcpio-archiso.conf
 rm -r /etc/initcpio
-rm /usr/bin/arcolinux-all-cores.sh
-rm /usr/bin/arcolinux-cleanup.sh
+arco-reflector
+rm /usr/local/bin/arcolinux-all-cores.sh
+rm /usr/local/bin/arcolinux-cleanup.sh
