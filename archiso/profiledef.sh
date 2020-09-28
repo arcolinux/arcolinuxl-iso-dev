be86cae210f87c1c763d327104b3ba7e47a8e645
Marco ObaID <marco.obaid@gmail.com>
2020-09-27 21:02:08 -0500

    Correct arch variable in profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -11,5 +11,5 @@ work_dir=work
 out_dir=out
 gpg_key=""
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
-arch=""
+arch="x86_64"
 pacman_conf="pacman.conf"
