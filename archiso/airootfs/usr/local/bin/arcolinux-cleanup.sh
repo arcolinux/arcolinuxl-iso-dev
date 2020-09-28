c28bba4f483d93da9abde599aa9592e045b3999c
Erik Dubois <erik.dubois@gmail.com>
2020-09-28 21:49:53 +0200

    remove mkinitcpio-archiso via calamares

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -5,18 +5,18 @@ echo "#################################"
 #usermod -s /usr/bin/bash root
 #cp -aT /etc/skel/ /root/
 echo "Permissions of important folders"
+echo "#################################"
 chmod 750 /root
 chmod 750 /etc/sudoers.d
 chmod 750 /etc/polkit-1/rules.d
 chgrp polkitd /etc/polkit-1/rules.d
 
 echo "Reflector"
+echo "#################################"
 reflector --threads 50 -l 100 -f 100 --number 20 --sort rate --save /etc/pacman.d/mirrorlist
 
-#echo "Clean Pacman cache"
-#pacman -Sc --noconfirm
-
 echo "Populate Core Team keys"
+echo "#################################"
 pacman-key --init
 pacman-key --populate archlinux
 pacman-key --populate arcolinux
@@ -34,15 +34,18 @@ pacman-key --lsign-key 18064BF445855549
 pacman-key --lsign-key 7EC1A5550718AB89
 
 echo " Copy grub Vimix theme (archiso deletes /boot when making the ISO)"
+echo "#################################"
 mkdir -p /boot/grub/themes
 cp -Rf /usr/share/grub/themes/Vimix /boot/grub/themes/
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Cleanup autologin root"
+echo "#################################"
 rm -rf /etc/systemd/system/getty@tty1.service.d
 
 #Original cleanup
 echo "Cleanup original files"
+echo "#################################"
 rm -f /etc/sudoers.d/g_wheel
 rm -rf /usr/share/backgrounds/xfce
 rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
@@ -50,7 +53,7 @@ rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
 rm /root/{.automated_script.sh,.zlogin}
 rm /usr/local/bin/arcolinux-all-cores.sh
 mv /etc/arcolinux-release /etc/lsb-release
-pacman -R mkinitcpio-archiso --noconfirm
+#pacman -R mkinitcpio-archiso --noconfirm
 mv /etc/mkinitcpio.d/arcolinux /etc/mkinitcpio.d/linux.preset
 rm /usr/local/bin/arcolinux-cleanup.sh
 echo "#################################"
