636669f0c6de009c9801cd6f8ff8261b193bb360
Erik Dubois <erik.dubois@gmail.com>
2020-09-29 09:19:18 +0200

    adding bluetooth settings

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -45,8 +45,12 @@ rm -rf /etc/systemd/system/getty@tty1.service.d
 
 echo "Fix for pamac icons not showing"
 zcat /usr/share/app-info/xmls/community.xml.gz | sed 's|<em>||g;s|<\/em>||g;' | gzip > "new.xml.gz"
-sudo mv new.xml.gz /usr/share/app-info/xmls/community.xml.gz
-sudo appstreamcli refresh-cache --force
+mv new.xml.gz /usr/share/app-info/xmls/community.xml.gz
+appstreamcli refresh-cache --force
+
+echo "Bluetooth improvements"
+sed -i "s/#AutoEnable=false/AutoEnable=true/g" /etc/bluetooth/main.conf
+echo 'load-module module-switch-on-connect' | sudo tee --append /etc/pulse/default.pa
 
 #Original cleanup
 echo "Cleanup original files"
