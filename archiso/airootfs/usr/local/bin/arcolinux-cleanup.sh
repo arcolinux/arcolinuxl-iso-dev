3cab7e944f83d092b07c52d7c9147524e50e6faa
Erik Dubois <erik.dubois@gmail.com>
2020-09-29 09:22:54 +0200

    lalayout

--- archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
+++ archiso/airootfs/usr/local/bin/arcolinux-cleanup.sh
@@ -44,11 +44,13 @@ echo "#################################"
 rm -rf /etc/systemd/system/getty@tty1.service.d
 
 echo "Fix for pamac icons not showing"
+echo "#################################"
 zcat /usr/share/app-info/xmls/community.xml.gz | sed 's|<em>||g;s|<\/em>||g;' | gzip > "new.xml.gz"
 mv new.xml.gz /usr/share/app-info/xmls/community.xml.gz
 appstreamcli refresh-cache --force
 
 echo "Bluetooth improvements"
+echo "#################################"
 sed -i "s/#AutoEnable=false/AutoEnable=true/g" /etc/bluetooth/main.conf
 echo 'load-module module-switch-on-connect' | sudo tee --append /etc/pulse/default.pa
 
