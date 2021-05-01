53806d1199ed73eed934152b29804e8ce8781cea
Erik Dubois <erik.dubois@gmail.com>
2021-05-01 14:48:36 +0200

    new archiso version

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="arcolinux-dev"
-iso_label="arcolinux-dev-v21.05.7"
+iso_name="arcolinuxl-dev"
+iso_label="arcolinuxl-dev-v21.05.7"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
 iso_version="v21.05.7"
